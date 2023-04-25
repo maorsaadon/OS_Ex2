29665b31b5c4c433163c308d950633d342c9bfbe
maorsaadon <105555708+maorsaadon@users.noreply.github.com>
2023-04-25 18:12:32 +0300

    wip

--- stshell.c
+++ stshell.c
@@ -8,6 +8,12 @@
 #include <string.h>
 #include <signal.h>
 
+// clear stshell screen
+void clear_screen()
+{
+    printf("\033[H\033[J");
+}
+
 int main()
 {
     int argc;
@@ -15,11 +21,25 @@ int main()
     char command[1024];
     char *token;
 
+    signal(SIGINT, SIG_IGN); // cancelling Ctrl+C
     while (1)
     {
-        printf("hello: ");
+        printf("\033[1;34mstshell> \033[0m"); // Blue
         fgets(command, 1024, stdin);
         command[strlen(command) - 1] = '\0'; // replace \n with \0
+
+        // clear the screen
+        if (strcmp(command, "clear") == 0)
+        {
+            clear_screen();
+            continue;
+        }
+
+        // exit the program
+        if (strcmp(command, "exit") == 0)
+        {
+            exit(0);
+        }
 
         /* parse command line */
         argc = 0;
@@ -34,11 +54,12 @@ int main()
 
         /* Is command empty */
         if (argv[0] == NULL)
+        {
             continue;
+        }
 
-        signal(SIGINT, SIG_IGN); // cancling Ctrl+C
-
-        int rediract_exist = 0;
+        int single_rediract_exist = -1;
+        int double_rediract_exist = -1;
         int pipe_exist = 0;
         int pipes[] = {-1, -1};
 
@@ -47,15 +68,15 @@ int main()
         if (id1 == 0)
         {
             signal(SIGINT, SIG_DFL); // exist Ctrl+C
-            for(int i = 0; i < argc; i++)
+            for (int i = 0; i < argc; i++)
             {
                 if (strcmp(argv[i], ">") == 0)
                 {
-                    rediract_exist = 1;
+                    single_rediract_exist = i;
                 }
                 if (strcmp(argv[i], ">>") == 0)
                 {
-                    rediract_exist = 2;
+                    double_rediract_exist = i;
                 }
                 if (strcmp(argv[i], "|") == 0)
                 {
@@ -64,17 +85,116 @@ int main()
                 }
             }
 
-            if(rediract_exist == 0 && pipe_exist == 0){
-                printf("command not found\n");
+            if (single_rediract_exist != -1)
+            {
+                if (freopen(argv[single_rediract_exist + 1], "w", stdout) == NULL)
+                {
+                    printf("freopen failed\n");
+                    return 1;
+                }
+                argv[single_rediract_exist] = NULL;
+            }
+            if (double_rediract_exist != -1)
+            {
+                if (freopen(argv[double_rediract_exist + 1], "a", stdout) == NULL)
+                {
+                    printf("freopen failed\n");
+                    return 1;
+                }
+                argv[double_rediract_exist] = NULL;
+            }
+            if (pipes[0] != -1)
+            {
+                argv[pipes[0]] = NULL;
+
+                // execute the first command
+                if (fork() == 0)
+                {
+                    // redirect stdout into a text file
+                    if (freopen("pipes.txt", "w", stdout) == NULL)
+                    {
+                        printf("freopen failed\n");
+                        return 1;
+                    }
+
+                    // execute the first command so its output goes to the file
+                    if (execvp(argv[0], argv) == -1)
+                    {
+                        printf("%s: command not found\n", argv[0]);
+                        return 1;
+                    }
+                }
+                wait(NULL);
+
+                // two pipelines
+                if (pipes[1] != -1)
+                {
+                    argv[pipes[1]] = NULL;
+
+                    if (fork() == 0)
+                    {
+                        // redirect stdin into a text file
+                        if (freopen("pipes1.txt", "r", stdin) == NULL)
+                        {
+                            printf("freopen failed\n");
+                            return 1;
+                        }
+
+                        // redirect stdout into a text file
+                        if (freopen("pipes2.txt", "w", stdout) == NULL)
+                        {
+                            printf("freopen failed\n");
+                            return 1;
+                        }
+
+                        // execute the second command so its input is from the file
+                        if (execvp(argv[pipes[0] + 1], argv + pipes[0] + 1) == -1)
+                        {
+                            printf("%s: command not found\n", argv[0]);
+                            return 1;
+                        }
+                    }
+                    wait(NULL);
+
+                    // redirect stdin into a text file
+                    if (freopen("pipes2.txt", "r", stdin) == NULL)
+                    {
+                        printf("freopen failed\n");
+                        return 1;
+                    }
+
+                    // execute the third command so its input is from the file
+                    if (execvp(argv[pipes[1] + 1], argv + pipes[1] + 1) == -1)
+                    {
+                        printf("%s: command not found\n", argv[0]);
+                        return 1;
+                    }
+                }
+
+                // one pipe
+                else
+                {
+                    // redirect stdin into a text file
+                    if (freopen("pipes1.txt", "r", stdin) == NULL)
+                    {
+                        printf("freopen failed\n");
+                        return 1;
+                    }
+
+                    // execute the second command so its input is from the file
+                    if (execvp(argv[pipes[0] + 1], argv + pipes[0] + 1) == -1)
+                    {
+                        printf("%s: command not found\n", argv[0]);
+                        return 1;
+                    }
+                }
+            }
+            else if (execvp(argv[0], argv) == -1)
+            {
+                printf("%s: command not found\n", argv[0]);
                 return 1;
             }
-            pid_t id2 = fork();
-            if(id2 == 0){
-                
-            }
-
-            execvp(argv[0], argv);
-            wait(NULL);
         }
+        wait(NULL);
     }
 }
